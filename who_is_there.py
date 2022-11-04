fcd871cd90307b24f47103946ffe92b057219b15
Pheonix <daluudeze@gmail.com>
2022-11-04 10:04:00 +0800

    this is for a single line print with more than one value on one line

--- who_is_there.py
+++ who_is_there.py
@@ -1,1 +1,4 @@
-
+number = input("Give me a number: ")
+group = input("Give me a collective noun for a group of things: ")
+thing = input("Give me the name of a weird or wacky thing:")
+print("No i don't think that",number, "is a",group, "of", thing,". Thats's just odd.")
